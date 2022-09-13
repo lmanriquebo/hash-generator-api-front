1c4b7218a78dafae695a3bb06404a4ee56c8edb9
Luis Manrique <lmanriquebo@gmail.com>
2022-09-12 23:51:48 -0500

    Ajuste interfaz y definición de evento de alerta entre otros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+$(function(){
+    //Adicion de eventos en input de bootstrap que no trae previamente desarrollado
+    $('.custom-file-input').on('change',function(){
+        //Captura el nombre del documento seleccionado
+        var fileName = !$(this).val() ? "Seleccionar Archivo" : $(this).prop("files")[0].name;
+        //Cambia el texto en el label por el nombre capturado
+        $(this).next('.custom-file-label').addClass("selected").html(fileName);
+    })
+})
+
 function mostrarFormulario(_id) {
     if (_id == "cont-generar-hash") {
         document.getElementById(_id).style.display = "block";
@@ -12,3 +22,70 @@ function mostrarFormulario(_id) {
     }
 }
 
+/**
+ * @function alertHash
+ * @desciption Genera la alerta con la información pertinente
+ * @param {*} action 
+ * @param {*} hash 
+ * @return events
+ */
+function alertHash(action, hash = ""){
+    //Crea el objeto con los datos necesarios para la alerta por defecto
+    let data = {
+        icon: "fa-circle-check",
+        text: "Se ha generado exitosamente.",
+        hash: "<strong>Hash:</strong> "+hash,
+        alert: "alert-"+action
+    };
+    //Dependiendo de la accion enviada, cambiará algunos valores de los datos generados en la alerta
+    switch (action) {
+        case "success":
+            data.icon = "fa-circle-check";
+            data.text = "<strong>Genial!!</strong>, se ha generado exitosamente.";
+            break;
+        
+        case "danger":
+            data.icon = "fa-square-xmark";
+            data.text = "<strong>Ups!!</strong>, no se pudo generar.";
+            break;
+        
+        case "warning":
+            data.icon = "fa-triangle-exclamation";
+            data.text = "<strong>Uy!</strong>, presenta novedades en la generación del hash";
+            break;
+    }
+
+    //Elimina las clases que muestran el icono en la alerta y añade el icono nuevo
+    $("#alertHash > .icon > i").removeClass(function(index,currentclass){
+        let dataClass = currentclass.split(" ");
+        removeSpecificClass($(this),dataClass,"fa-");    
+    }).addClass(data.icon);
+
+    //Reescribe el mensaje en la alerta y el hash
+    $("#alertHash > .icon > .text").html(data.text);
+    $("#alertHash > .icon > .hash").html(data.hash);
+
+    //Elimina las clases que muestran el tipo de alerta y añade el nuevo tipo y la muestra
+    $("#alertHash").removeClass(function(index,currentclass){
+        let dataClass = currentclass.split(" ");
+        removeSpecificClass($(this),dataClass,"alert-");     
+    }).addClass(data.alert+" show");
+}
+
+/**
+ * @function removeSpecificClass
+ * @desciption Remueve clases especificas basados en coincidencias de texto
+ * @param {*} objet 
+ * @param {*} dataClass 
+ * @param {*} textSearch 
+ */
+function removeSpecificClass(objet,dataClass,textSearch){
+    //Recorre las clases enviadas
+    $.each(dataClass, function (indexInArray, valueOfElement) {
+        //Valida si coinciden con el texto enviado
+        if(valueOfElement.indexOf(textSearch) > -1){
+            //Elimina del objeto la clase que coincide
+            objet.removeClass(valueOfElement);
+        }
+    });
+}
